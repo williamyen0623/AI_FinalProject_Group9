f3bcfdd092b4dea8a6db4fc33f6c3c218b46c447
williamyen0623 <a0988977340@gmail.com>
2022-12-13 16:00:54 +0800

    edit sqlite.py create table with ID

--- sqlite.py
+++ sqlite.py
@@ -1,7 +1,42 @@
 import sqlite3
 
-food_db_conn = sqlite3.connect('food_db')
-print("Datebase open success")
+#connect database
+try:
+    food_db_conn = sqlite3.connect('food_db')
+except:
+    print("DB connect fail")
+else:
+    print("DB connect success")
+
+#create table and set attributes
+
 food_db_c = food_db_conn.cursor()
-food_db_conn.commit()
-food_db_conn.close()
+try:
+    food_db_c.execute(
+    '''
+    CREATE TABLE FOODLIST
+    (
+        ID TEXT PRIMARY KEY NOT NULL
+    );
+    '''
+)
+except:
+    print("DB TABLE create fail")
+else:
+    print("DB TABLE create success")
+
+#commit database
+try:
+    food_db_conn.commit()
+except:
+    print("DB commit fail")
+else:
+    print("DB commit success")
+
+#close database
+try:
+    food_db_conn.close()
+except:
+    print("DB close fail")
+else:
+    print("DB close success")
