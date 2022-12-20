e0a231c2416075da6ba902f50c03a84d0d4e9c1c
williamyen0623 <a0988977340@gmail.com>
2022-12-20 14:28:47 +0800

    update

--- app.py
+++ app.py
@@ -31,6 +31,10 @@ def callback():
 
     return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+		message = text=event.message.text
+		line_bot_api.reply_message(event.reply_token, TextSendMessage(text = message))
 
 # handle msg
 @handler.add(MessageEvent)
