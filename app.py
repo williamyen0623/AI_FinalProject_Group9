d67ed36d6146410c36eb89fd7df772722c152af7
williamyen0623 <a0988977340@gmail.com>
2022-12-20 14:25:26 +0800

    update

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from linebot.models import (
     TextMessage,
     TextSendMessage,
 )
+from azure_speech_service import *
+import json
 import re
 
 app = Flask(__name__)
@@ -30,11 +32,52 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = text = event.message.text
-    line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=message))
+# handle msg
+@handler.add(MessageEvent)
+def handle_something(event):
+    if event.message.type == 'audio':
+        handle_audio_message(event)
+    if event.message.type == 'text':
+        recrive_text = event.message.text
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = recrive_text))
+
+
+style = ['中式', '日式', '韓式', '泰式', '美式', '歐式']
+meal = ['早餐', '午餐', '晚餐']
+day = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
+review_number = ['不限定', '0到200', '201到500', '501到1000', '1000以上']
+review_star = ['不設限', '4', '4.3', '4.5', '4.7', '5']
+choices_list = [style, meal, day, review_number, review_star]
+choices_str = ['style', 'meal', 'day', 'review_number', 'review_star']
+user_choices = {'style': '', 'meal': '', 'day': '', 'review_number': '', 'review_star': ''}
+query_message = {   'style': '請問是要查詢哪種料理風格的餐廳呢？\n中式、日式、韓式、泰式、美式、歐式',
+                                    'meal': '請問是要查詢早餐、午餐還是晚餐的餐廳呢？',
+                                    'day': '請問是要查詢星期一到日哪天的餐廳呢？',
+                                    'review_number': '請問是要查詢幾個評論以上的餐廳呢？\n不限定、0到200、201到500、501到1000、1000以上',
+                                    'review_star': '請問是要查詢幾顆星以上的餐廳呢？\n不設限、4、4.3、4.5、4.7、5'
+                                    }
+    
+def handle_audio_message(event):
+    message_content = line_bot_api.get_message_content(event.message.id)
+    filename_wav = 'temp_audio.wav'
+    message_to_wav(message_content, filename_wav)
+    text = transcribe_from_file(filename_wav)
+    print('Transcribe:', text)
+    if '找餐廳' in text:
+        user_choices = {'style': '', 'meal': '', 'day': '', 'review_number': '', 'review_star': ''}
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '料理風格、早午晚餐、星期幾、評論數、評論星星'))
+    for choice in choices_list:
+        for c in choice:
+            if c in text:
+                user_choices[choices_str[choices_list.index(choice)]] = c 
+                break
+    for choice in choices_str:
+        if user_choices[choice] == '':
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = query_message[choice]))
+            return
+    print('user_choices:', user_choices)
+    # call service
+    user_choices = {'style': '', 'meal': '', 'day': '', 'review_number': '', 'review_star': ''}
 
 
 if __name__ == "__main__":
---
+++ azure_speech_service.py
@@ -0,0 +1,37 @@
+# azure_speech_service.py
+
+
+import azure.cognitiveservices.speech as speechsdk
+import os
+
+from line_chatbot_api import *
+
+# (Resource Management -> Keys and Endpoint -> Key 1 or Key 2, Location/Region) subscription key and region for speech service
+speech_config = speechsdk.SpeechConfig(subscription="YourSpeechKey", region="eastus")
+speech_config.speech_recognition_language = "zh-TW"
+
+def message_to_wav(message_content, filename_wav):
+    filename_mp3 = 'temp_audio.mp3'
+    with open(filename_mp3, 'wb') as fd:
+        for chunk in message_content.iter_content():
+            fd.write(chunk)
+    # -i : set input file name, -y : overwrite output file if it exists, -loglevel quiet : suppress ffmpeg log output
+    os.system(f'ffmpeg -y -i {filename_mp3} {filename_wav} -loglevel quiet')
+
+def transcribe_from_file(filename_wav):
+    audio_input = speechsdk.AudioConfig(filename = filename_wav)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config = speech_config, audio_config = audio_input)
+    result = speech_recognizer.recognize_once_async().get()
+
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        return result.text
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print(f"No speech could be recognized: {result.no_match_details}")
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print(f"Speech Recognition canceled: {cancellation_details.reason}")
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print(f"Error details: {cancellation_details.error_details}")
+            print("Did you set the speech resource key and region values?")
+    return "transcribe error"
+
